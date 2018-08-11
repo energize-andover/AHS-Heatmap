f02899da1e695a189415a66abb38b509c33dd008
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2018-08-11 18:33:12 -0400

    Switch to PyPDF2

--- ConvertSVG.py
+++
@@ -1,5 +0,0 @@
-import cairosvg
-import os
-
-cairosvg.svg2png(url=os.path.join('svg', 'Andover HS level 3.svg'),
-                 write_to=os.path.join('converted svgs', 'Andover HS level 3.png'))
