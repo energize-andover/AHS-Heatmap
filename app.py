a45b612dc86bcb5197741dd4267883b676e5ff20
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2018-10-29 10:05:54 -0400

    Change SVG update mechanism
    In the old system, the JavaScript would make an AJAX request to the site every 5 minutes, asking the server to regenerate the SVG, and then would reload the embedded SVG. This would be problematic if many users were on the site at once, as some unlucky clients may try requesting the SVG while it's still being generated, and get an error. I now wrote this new system that syncs up the server's time and the client's time on initialization, so that only 1 regenerate request is made every 5 minutes, by the server, and the server is given time to complete the SVG (1 minute) before the clients reload the SVG.

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from main import init, delete_temp_file
 from data_tools import init_data_tools, fill_all_rooms, update_air_data
 from flask import *
 import os
+import sched
+import time
+import datetime as dt
+import timeinterval
+import threading
 
 svg_flask_path = os.path.join('static', 'svg_and_conversions', "Andover-HS-level-3.svg")
 svg_output_path = svg_flask_path[0:-4] + "_filled_rooms.svg"
@@ -19,31 +24,56 @@ GREEN_VALUE = (70, 900)
 RED_VALUE = (80, 2000)
 
 init(svg_path, RED_VALUE, GREEN_VALUE, BLUE_VALUE)
-init_data_tools(rooms_and_sensors, 'energize.andoverma.us', '8000')
+init_data_tools(rooms_and_sensors, '10.12.4.98', '8000')
 
 is_temp_value = True  # True if the current SVG displays temperature
 
 fill_all_rooms(is_temp_value)  # First start with temperature
 
-app = Flask(__name__)
+scheduler = sched.scheduler(time.time, time.sleep)
 
 
-@app.route("/")
-def load_svg():
-    return render_template('svg_output_page.html', title='Andover HS Level 3',
-                           svg_path=svg_output_path.replace('\\', '/'))
-
-
-@app.route("/update_svg")
 def update_svg():
-    try:
-        update_air_data()
-        delete_temp_file()
-        fill_all_rooms(is_temp_value)
-        return json.dumps({'status': 'ok'})
-    except:
-        return json.dumps({'status': 'error'})
+    print("Updating svg...")
+    update_air_data()
+    delete_temp_file()
+    fill_all_rooms(is_temp_value)
+
+
+def start_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def load_svg():
+        return render_template('svg_output_page.html', title='Andover HS Level 3',
+                               svg_path=svg_output_path.replace('\\', '/'))
+
+    app.run()
+
+
+def start_svg_auto_updater():
+    second = dt.datetime.now().second + 1
+
+    while second != 60:
+        time.sleep(1)
+        second = dt.datetime.now().second + 1
+
+    print("Seconds synced")
+
+    minute = dt.datetime.now().minute % 10
+
+    while minute != 4 and minute != 9:
+        time.sleep(60)
+        minute = dt.datetime.now().minute % 10
+
+    print("Minutes synced\nStarting update thread...")
+
+    timeinterval.start(5 * 60 * 1000, update_svg)
+    update_svg()
 
 
 if __name__ == '__main__':
-    app.run()
+    update_thread = threading.Thread(target=start_svg_auto_updater)
+    update_thread.start()
+
+    start_app()
