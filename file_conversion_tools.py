f71adb73a45b9d9d6fc72d03b52bb8d7cd83e988
PlantsVsDan <Soccer.Ivanovich@gmail.com>
2018-12-26 19:25:41 -0500

    Make conversion commands friendly to all OSes

--- file_conversion_tools.py
+++ file_conversion_tools.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from sys import platform
+import time
 import os
 
 
@@ -8,8 +10,13 @@ def svg_to_png(svg_path, png_path, dpi):
 
     # run the command to convert the svg, adding '> /dev/null' on the end to silence the output by storing it into null
     options = '--without-gui --export-area-page --export-background="#ffffff"'
-    os.system('inkscape %s "%s" --export-dpi=%s --export-png="%s"  > /dev/null' % (
-        options, svg_path, dpi, png_path))
+
+    if platform == "linux" or platform == "linux2":
+        # run command to convert the svg, adding '> /dev/null' on the end to silence the output by storing it into null
+        os.system('inkscape %s "%s" --export-dpi=%s --export-png="%s"  > /dev/null' % (options, svg_path, dpi, png_path))
+    else:
+        os.system(
+            'inkscape %s "%s" --export-dpi=%s --export-png="%s"' % (options, svg_path, dpi, png_path))
 
     wait_for_creation(png_path)
 
@@ -18,9 +25,13 @@ def svg_to_pdf(svg_path, pdf_path):
     # Delete the file if it exists, as inkscape won't overwrite
     remove_file(pdf_path)
 
-    # run the command to convert the svg, adding '> /dev/null' on the end to silence the output by storing it into null
     options = '--without-gui --export-area-page'
-    os.system('inkscape %s "%s" --export-pdf="%s"  > /dev/null' % (options, svg_path, pdf_path))
+
+    if platform == "linux" or platform == "linux2":
+        # run command to convert the svg, adding '> /dev/null' on the end to silence the output by storing it into null
+        os.system('inkscape %s "%s" --export-pdf="%s" > /dev/null' % (options, svg_path, pdf_path))
+    else:
+        os.system('inkscape %s "%s" --export-pdf="%s"' % (options, svg_path, pdf_path))
 
     wait_for_creation(pdf_path)
 
