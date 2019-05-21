76bb81daa2d65c64639244ffc41a6247fc8fdbb1
Daniel Ivanovich <dan@ivanovi.ch>
2019-05-21 12:31:41 -0400

    Begin sidenav animation

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -2,3 +2,15 @@ $('.navbar-burger').first().click(() => {
     $('.navbar-burger').first().toggleClass('is-active');
     $('#sidenav').toggleClass('active');
 });
+
+$('.sidenav-dropdown-title').each((indx, title) => {
+    let dropdown = $(title).parent();
+    let hidden = dropdown.children('.sidenav-dropdown-hidden').first();
+
+    dropdown.mouseenter(() => {
+        hidden.slideDown();
+    });
+    dropdown.mouseleave(() => {
+        hidden.slideUp();
+    });
+});
